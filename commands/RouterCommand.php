4b0a48db9318d6256e816fcf903e22f9d9ef5fe5
Milad Yousefi <yousefimiladdd@gmail.com>
2024-11-23 15:21:59 +0330

    first commmit

--- commands/RouterCommand.php
+++ commands/RouterCommand.php
@@ -8,42 +8,45 @@ use Symfony\Component\Console\Output\OutputInterface;
 
 class RouterCommand extends Command
 {
-    protected static $defaultName = 'make:model';
+    protected static $defaultName = 'make:router';
 
     protected function configure()
     {
         $this
-            ->setDescription('Generate a model and ensure its corresponding table exists in the database.');
-        }
+            ->setDescription('Generate a Router.php file based on a stub file.');
+    }
 
     protected function execute(InputInterface $input, OutputInterface $output): int
     {
+        // Debugging output
+        $output->writeln("<info>Running RouterCommand...</info>");
+
         // Get the addon name from the parent directory
         $addonName  = basename(getcwd());
 
         $currentDir = getcwd();
-        $modelFilePath = $currentDir . DIRECTORY_SEPARATOR.'Router.php';
+        $routerFilePath = $currentDir . DIRECTORY_SEPARATOR . 'src' . DIRECTORY_SEPARATOR . 'Router.php';  // Ensure correct path
 
         // Load and process the stub file
-        $stubPath = __DIR__ . '/router.stub';  // Path to the stub file
+        $stubPath = __DIR__ . '/../router.stub';  // Path to the stub file
         if (!file_exists($stubPath)) {
-            $output->writeln("<error>Model stub file not found: $stubPath</error>");
+            $output->writeln("<error>Router stub file not found: $stubPath</error>");
             return Command::FAILURE;
         }
 
         // Read the stub and replace placeholders
         $stubContent = file_get_contents($stubPath);
-        $modelStub = str_replace(
+        $routerStub = str_replace(
             ['{{addonName}}'],
             [$addonName],
             $stubContent
         );
 
-        // Write the model file
-        if (file_put_contents($modelFilePath, $modelStub)) {
-            $output->writeln("<info>Created model: $modelFilePath</info>");
+        // Write the router file
+        if (file_put_contents($routerFilePath, $routerStub)) {
+            $output->writeln("<info>Created Router.php file: $routerFilePath</info>");
         } else {
-            $output->writeln("<error>Failed to create model file. Please check permissions.</error>");
+            $output->writeln("<error>Failed to create Router.php file. Please check permissions.</error>");
             return Command::FAILURE;
         }
 
