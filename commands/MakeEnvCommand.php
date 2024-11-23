c8d741b6c747df6d11b37f60388dd5b8f506c6ca
Milad Yousefi <yousefimiladdd@gmail.com>
2024-11-23 11:45:07 +0330

    first commmit

---
+++ commands/MakeEnvCommand.php
@@ -0,0 +1,53 @@
+<?php
+
+require __DIR__ . '/../vendor/autoload.php';
+
+use Symfony\Component\Console\Application;
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+use Illuminate\Support\Facades\File;
+
+class MakeEnvCommand extends Command
+{
+    protected static $defaultName = 'make:env';
+
+    protected function configure()
+    {
+        $this
+            ->setDescription('Create a .env file from .env.example if it does not already exist.');
+    }
+
+    protected function execute(InputInterface $input, OutputInterface $output): int
+    {
+        $currentDir = getcwd(); // Get the current working directory
+        $envPath = $currentDir . DIRECTORY_SEPARATOR . '.env';
+        $envExamplePath = $currentDir . DIRECTORY_SEPARATOR . '.env.example';
+
+        // Check if .env file already exists
+        if (file_exists($envPath)) {
+            $output->writeln('<info>.env file already exists. No action taken.</info>');
+            return Command::SUCCESS;
+        }
+
+        // Check if .env.example exists
+        if (!file_exists($envExamplePath)) {
+            $output->writeln('<error>.env.example file does not exist. Unable to create .env file.</error>');
+            return Command::FAILURE;
+        }
+
+        // Copy .env.example to .env
+        try {
+            if (copy($envExamplePath, $envPath)) {
+                $output->writeln('<info>.env file successfully created from .env.example.</info>');
+                return Command::SUCCESS;
+            } else {
+                $output->writeln('<error>Failed to create .env file. Please check file permissions.</error>');
+                return Command::FAILURE;
+            }
+        } catch (\Exception $e) {
+            $output->writeln('<error>An error occurred: ' . $e->getMessage() . '</error>');
+            return Command::FAILURE;
+        }
+    }
+}
